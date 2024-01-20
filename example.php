0a1527887572c4a6edf37631715b196432a2fd77
Russell Purinton <russell.purinton@gmail.com>
2024-01-20 05:15:03 -0500

    Updated Example

--- example.php
+++ example.php
@@ -4,8 +4,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use RPurinton\HTTPS\HTTPSRequest;
 
-// Return the response as a string
-$response_string = (string)(new HTTPSRequest([
+// Define the options for the request
+$options = [
     'url' => 'https://raw.githubusercontent.com/rpurinton/https/master/example.json',
     'method' => 'GET',
     'headers' => [
@@ -13,29 +13,14 @@ $response_string = (string)(new HTTPSRequest([
             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
     ],
     'body' => '',
-]));
+];
+
+// Return the response as a string
+$response_string = HTTPSRequest::fetch($options);
 echo "Response as String: $response_string\n";
 
-// Return the response as an array
-$response_array = (array)(new HTTPSRequest([
-    'url' => 'https://raw.githubusercontent.com/rpurinton/https/master/example.json',
-    'method' => 'GET',
-    'headers' => [
-        'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' .
-            'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
-    ],
-    'body' => '',
-]));
+$response_array = json_decode($response_string, true);
 echo "Response as Array: " . print_r($response_array, true) . "\n";
 
-// Return the response as an object
-$response_object = (object)(new HTTPSRequest([
-    'url' => 'https://raw.githubusercontent.com/rpurinton/https/master/example.json',
-    'method' => 'GET',
-    'headers' => [
-        'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' .
-            'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
-    ],
-    'body' => '',
-]));
+$response_object = json_decode($response_string);
 echo "Response as Object: " . print_r($response_object, true) . "\n";
